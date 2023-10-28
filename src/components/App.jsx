4d85a89472fd52af0c1519a0af1d3b042150b16e
Rhdshfeyhccfu <130246853+Mykola1612@users.noreply.github.com>
2023-10-28 03:17:12 +0300

    Summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,10 @@ export class App extends React.Component {
     element: null,
     loader: false,
     inputValue: '',
+    totalHits: null,
+    per_page: 12,
+    page: 1,
+    howPage: null,
   };
 
   fetchPhoto = async query => {
@@ -20,18 +24,20 @@ export class App extends React.Component {
       });
       if (!query) {
         const { data } = await axios.get(
-          'https://pixabay.com/api/?q=&page=1&key=39488259-261aead24914c58cf003fef3e&image_type=photo&orientation=horizontal&per_page=12&page=1'
+          `https://pixabay.com/api/?q=&page=1&key=39488259-261aead24914c58cf003fef3e&image_type=photo&orientation=horizontal&per_page=${this.state.per_page}&page=${this.state.page}`
         );
         this.setState({
           element: data.hits,
+          totalHits: data.totalHits,
         });
         return;
       }
       const { data } = await axios.get(
-        `https://pixabay.com/api/?q=${query}&page=1&key=39488259-261aead24914c58cf003fef3e&image_type=photo&orientation=horizontal&per_page=12&page=1`
+        `https://pixabay.com/api/?q=${query}&page=1&key=39488259-261aead24914c58cf003fef3e&image_type=photo&orientation=horizontal&per_page=${this.state.per_page}&page=${this.state.page}`
       );
       this.setState({
         element: data.hits,
+        totalHits: data.totalHits,
       });
     } catch (error) {
       console.error('Ошибка при загрузке данных:', error);
@@ -57,6 +63,31 @@ export class App extends React.Component {
     );
   };
 
+  handleOnClick = pageNumber => {
+    const fetchPhotoNewPage = async () => {
+      try {
+        this.setState({
+          loader: true,
+        });
+        const { data } = await axios.get(
+          `https://pixabay.com/api/?q=${this.state.inputValue}&page=1&key=39488259-261aead24914c58cf003fef3e&image_type=photo&orientation=horizontal&per_page=${this.state.per_page}&page=${pageNumber}`
+        );
+        this.setState(prevState => ({
+          element: [...prevState.element, ...data.hits],
+        }));
+      } catch (error) {
+        console.error('Ошибка при загрузке данных:', error);
+      } finally {
+        this.setState({
+          loader: false,
+        });
+      }
+    };
+    fetchPhotoNewPage();
+    this.setState({
+      page: pageNumber,
+    });
+  };
   render() {
     return (
       <div>
@@ -67,9 +98,18 @@ export class App extends React.Component {
             <ImageGalleryItem elements={this.state.element} />
           </ImageGallery>
         )}
-        {this.state.element !== null && this.state.element.length !== 0 && (
-          <Button />
-        )}
+        {this.state.element !== null &&
+          this.state.totalHits > this.state.per_page &&
+          Math.ceil(this.state.totalHits / this.state.per_page) !==
+            this.state.page &&
+          this.state.element.length !== 0 && (
+            <Button
+              page={this.state.page}
+              per_page={this.state.per_page}
+              totalHits={this.state.totalHits}
+              handleOnClick={this.handleOnClick}
+            />
+          )}
       </div>
     );
   }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import styles from './Button.module.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const Button = () => {
-  const handleOnClick = () => {
-    Notify.warning(
-      "We're sorry, but you've reached the end of search results."
-    );
+const Button = ({ page, per_page, totalHits, handleOnClick }) => {
+  const handleOnClickButton = () => {
+    const howPage = Math.ceil(totalHits / per_page);
+    console.log(howPage);
+    if (page > howPage) {
+      Notify.warning(
+        "We're sorry, but you've reached the end of search results."
+      );
+      return;
+    } else {
+      page += 1;
+      handleOnClick(page);
+      return;
+    }
   };
 
   return (
-    <section>
-      <button className={styles.Button} onClick={handleOnClick}>
+    <section className={styles.containerButton}>
+      <button className={styles.Button} onClick={handleOnClickButton}>
         Load more
       </button>
     </section>
